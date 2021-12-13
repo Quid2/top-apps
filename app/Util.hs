1fc41f002cb51956f6e3fa73b908889b27c8a572
Pasqualino Titto Assini <tittoassini@gmail.com>
2021-12-13 15:58:56 +0100

    remove dep on Util

--- app/Util.hs
+++ app/Util.hs
@@ -1,8 +1,13 @@
 module Util where
 
-import           Network.Top.Util
+import           Control.Monad.IO.Class
+import           Network.Top.Util       (dbg)
 
+noRequestLogger :: Monad m => p1 -> p2 -> p3 -> m ()
 noRequestLogger _ _ _ = return ()
 
+requestLogger :: (MonadIO m, Show a1, Show a2, Show a3) => a1 -> a2 -> a3 -> m ()
 requestLogger req status maybeL =
   dbg ["Request:", show req, show status, show maybeL]
+
+
