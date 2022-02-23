a900968f75ec51c97a3c2cc1112b8cd7243f8335
Pasqualino Titto Assini <tittoassini@gmail.com>
2022-02-23 16:13:22 +0100

    additional www tests

--- app/testApp.hs
+++ app/testApp.hs
@@ -15,11 +15,19 @@ import           Test
 
 {-
 >>> 3 + 3
-
 -}
 
 main :: IO ()
 -- main = run [ovhTest "KS-17"] --
+-- [ovhTest "KS-1",ovhTest "KS-2"] ++
 
--- [ovhTest "KS-1",ovhTest "KS-2"] ++ 
-main = run $ map wwwTest [("https://quid2.org","Flat"),("http://quid2.org","Flat"),("http://kamus.it/","Assini"),("http://quid2.org/app/ui","channels"),("http://vip.quid2.org/","top-router"),("http://vip.quid2.org:7000/","BLOB")]
+main = run $ map wwwTest $
+    map (\n -> ("http://"++ n,"Flat")) ["quid2.biz","quid2.com","quid2.info","quid2.org","quicquid.org","golo.so"]
+    ++ [("https://quid2.org","Flat")
+        ,("http://kamus.it/","Assini")
+        ,("http://massimoassini.quicquid.org/","Massimo Assini")
+        ,("http://quid2.org/app/ui","channels")
+        ,("http://quid2.net/","top-router")
+        ,("http://quid2.net:7000/","BLOB")
+        ,("http://vip.quid2.org/","top-router")
+        ,("http://vip.quid2.org:7000/","BLOB")]
