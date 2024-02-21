59e8d2d7cd721d9c01ba95cdd309f9469eee13b0
Pasqualino Titto Assini <tittoassini@gmail.com>
2024-02-21 17:24:36 +0100

    removed test on quicquid.org

--- app/testApp.hs
+++ app/testApp.hs
@@ -20,7 +20,7 @@ import           Test
 >>> 3 + 3
 -}
 
-t = run [wwwTest ("http://vip.quid2.org/","top-router")]
+t = run [wwwTest ("http://net.quid2.org/","top-router")]
 
 main :: IO ()
 -- main = run [ovhTest "KS-17"] --
@@ -30,7 +30,7 @@ main = run $ map wwwTest $
     map (\n -> ("http://"++ n,"Flat")) ["quid2.biz","quid2.com","quid2.info","quid2.org","quicquid.org","golo.so"]
     ++ [("https://quid2.org","Flat")
         ,("http://kamus.it/","Assini")
-        ,("http://massimoassini.quicquid.org/","Massimo Assini")
+        -- ,("http://massimoassini.quicquid.org/","Massimo Assini")
         ,("http://quid2.org/app/ui","channels")
         ,("http://quid2.net/","top-router")
         ,("http://quid2.net:7000/","ADT")
