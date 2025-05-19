d1b76a9f72f124b68ff3eb4d2fd8cabcb3d9a104
Pasqualino Titto Assini <tittoassini@gmail.com>
2025-05-19 15:00:00 +0200

    modified test

--- src/Test.hs
+++ src/Test.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
@@ -6,12 +5,12 @@
 module Test
   ( runTests,
     wwwTest,
+    module Test.Types,
   )
 where
 
 import Control.Concurrent.Async (async, waitCatch)
 import Control.Retry
-
 import qualified Data.Map as M
 import Data.Maybe (catMaybes, isJust, maybeToList)
 import Data.String (IsString (fromString))
@@ -21,15 +20,13 @@ import Network.Top (async, seconds, threadDelay)
 import Repo.Memory ()
 import System.Environment (getArgs)
 import System.Exit (ExitCode)
-import System.Timeout (timeout)
 import Test.OVH
-import Test.Types (Test (check, name, source, timeoutInSecs))
+import Test.Types
 import Test.WWW (notContains, wwwTest, wwwTest_)
 import Util
 
 -- main = run [ovhTest "KS-17"] --
 -- [ovhTest "KS-1",ovhTest "KS-2"] ++
-
 
 {-
 Run a list of tests concurrently and return list of failures when all are done or timed out
@@ -50,16 +47,16 @@ runTests tests = do
 -- Nothing
 -- >>> runTest $ wwwTest ("http://net.quid2.org/","top-routery")
 -- Just "http://net.quid2.org/: does not contain \"top-routery\""
-runTest t = do
-  r <- timeout (seconds $ timeoutInSecs t) (source t)
-  return $ fmap (\r -> name t <> ": " <> r) $ case r of
-    Nothing -> Just "Timeout"
-    Just c -> check t c
-  where
-    -- chk :: Either SomeException (Maybe B8.ByteString) -> Either B8.ByteString B8.ByteString
-    chk (Left exp) = Left (fromString . show $ exp)
-    chk (Right Nothing) = Left "Test Timeout"
-    chk (Right (Just s)) = Right s
+-- runTest t = do
+--   r <- timeout (seconds $ timeoutInSecs t) (apply t)
+--   return $ fmap (\r -> name t <> ": " <> r) $ case r of
+--     Nothing -> Just "Timeout"
+--     Just c -> check t c
+--   where
+--     -- chk :: Either SomeException (Maybe B8.ByteString) -> Either B8.ByteString B8.ByteString
+--     chk (Left exp) = Left (fromString . show $ exp)
+--     chk (Right Nothing) = Left "Test Timeout"
+--     chk (Right (Just s)) = Right s
 
 -- t = run logger [wwwTest ("https://quid2.org", "Flat")]
 
@@ -79,42 +76,3 @@ runTest t = do
 --       failedTests <- filter (isJust . snd) <$> runTests tests
 --       unless (null failedTests) $ void $ err logger (show . map (\(name, Just err) -> unwords [name, err]) $ failedTests)
 --       threadDelay (seconds 60)
-
--- runTests :: (Traversable t) => t Test -> IO (t (String, Maybe String))
--- runTests = mapM runTest
-
-{-
->>> sequence [return $ Just "ok",return $ Just "badSafeIndex"]
-Just ["ok","badSafeIndex"]
-
->>> sequence [Just "ok",Just "badSafeIndex",Nothing]
-Nothing
--}
--- Returns (test name ,Nothing if ok or Just error)
--- runTest :: Test -> IO (String, Maybe String)
--- runTest t = do
---   rs <- sequence <$> replicateM 3 (runTest_ t)
---   return (name t, head <$> rs)
-
--- r t = (name t,) <$> go 2
--- runTest t = (name t,) <$> go 2
---   where
---     go 0 = runTest_ t
---     go n = do
---       rt <- runTest_ t
---       case rt of
---         Nothing -> return Nothing
---         Just _ -> threadDelay (secs 30) >> go (n - 1)
-
--- runTest_ :: Test -> IO (Maybe String)
--- runTest_ t = do
---   r <- chk <$> (async (timeout (seconds $ timeoutInSecs t) (source t)) >>= waitCatch)
---   -- print r
---   return $ case r of
---     Left e -> Just e
---     Right c -> check t c
---   where
---     -- chk :: Either SomeException (Maybe B8.ByteString) -> Either B8.ByteString B8.ByteString
---     chk (Left exp) = Left (fromString . show $ exp)
---     chk (Right Nothing) = Left "Test Timeout"
---     chk (Right (Just s)) = Right s
--- src/Util.hs
+++ src/Util.hs
@@ -1,19 +1,30 @@
-module Util(noRequestLogger,msecs,secs,minutes) where
+module Util (noRequestLogger, msecs, secs, minutes, periodically, voidFork) where
 
-import           Control.Monad.IO.Class
-import           Network.Top.Util       (dbg)
+import Control.Concurrent
+import Control.Monad
+import Control.Monad.IO.Class
+import Network.Top.Util (dbg)
+import Prelude
 
-noRequestLogger :: Monad m => p1 -> p2 -> p3 -> m ()
+voidFork = void . forkIO
+
+noRequestLogger :: (Monad m) => p1 -> p2 -> p3 -> m ()
 noRequestLogger _ _ _ = return ()
 
 requestLogger :: (MonadIO m, Show a1, Show a2, Show a3) => a1 -> a2 -> a3 -> m ()
 requestLogger req status maybeL =
   dbg ["Request:", show req, show status, show maybeL]
 
+msecs :: (Num a) => a -> a
 msecs t = t * 1000
 
+secs :: (Num a) => a -> a
 secs t = t * msecs 1000
 
+minutes :: (Num a) => a -> a
 minutes t = t * secs 60
 
-
+periodically :: Int -> IO a -> IO ()
+periodically numSeconds action = voidFork $ forever $ do
+  action
+  threadDelay $ secs numSeconds
