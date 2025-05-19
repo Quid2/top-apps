f1e959075658bbba97c2d9e38f2aa221a2535e6d
Pasqualino Titto Assini <tittoassini@gmail.com>
2025-05-19 14:11:06 +0200

    removed unused

--- app/Sensor/Model1.hs
+++
@@ -1,8 +0,0 @@
-{-# Language DeriveGeneric ,DeriveAnyClass #-}
-module Sensor.Model1 where
-
-import ZM
-
--- A rather asinine data model
-data MySensor = MySensor Int -- These are Celsius by the way!
-              deriving (Eq, Ord, Read, Show, Generic, Flat, Model)
